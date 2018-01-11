532f0b728afd8a5755721d594db74f52042369ad
Kovalev Valdislav <veosett@gmail.com>
2018-01-11 20:49:42 +0600

    fix stack

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "stack.h"
-#include <memory.h>
 
 int main() {
 
     Stack s;
     int some = 21;
-    NewStack(&s, sizeof(int), 8);
+    NewStack(&s, sizeof(int), 4);
     PushStack(&s, &some);
     some = 2;
     PushStack(&s, &some);
     some = 14;
     PushStack(&s, &some);
-    PopStack(&s);
     some = 6;
     PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PushStack(&s,&some);
+    PopStack(&s);
+    PopStack(&s);
+    PopStack(&s);
+    PopStack(&s);
 
     DeleteStack(&s);
 
-    printf("Hello, World!\n");
     return 0;
 }
--- stack.c
+++ stack.c
@@ -4,38 +4,39 @@
 #include <memory.h>
 
 void NewStack(Stack *s, size_t elemSize, size_t length) {
-    s->length = length;
-    s->elemSize = elemSize;
-    s->logLenght = 0;
-    s->elems = malloc(elemSize * length);
-    assert(s->elems != NULL);
+  s->length = length;
+  s->elemSize = elemSize;
+  s->logLenght = 0;
+  s->elems = malloc(16);
+  assert(s->elems != NULL);
 }
 
 int PushStack(Stack* s, void* elemAddr) {
-    if (s->length == s->logLenght) {
-        s->length *= 2;
-        s->elems = realloc(s->elems, s->length);
-    }
-    void* logElem = (char*)s->elems + s->elemSize * s->logLenght;
-    memcpy(logElem, elemAddr, s->logLenght);
-    s->logLenght++;
+  if (s->length == s->logLenght) {
+    s->length *= 2;
+    s->elems = realloc(s->elems, s->length * s->elemSize);
+    assert(s->elems != NULL);
+  }
+  void* logElem = (char*)s->elems + s->elemSize * s->logLenght;
+  memcpy(logElem, elemAddr, s->elemSize);
+  s->logLenght++;
 
-    return 0;
+  return 0;
 }
 
 int PopStack(Stack *s) {
-    if (IsEmpty(s)) {
-        return -1;
-    }
+  if (IsEmpty(s)) {
+    return -1;
+  }
 
-    s->logLenght--;
-    return 0;
+  s->logLenght--;
+  return 0;
 }
 
 bool IsEmpty(Stack *s) {
-    return s->logLenght == 0;
+  return s->logLenght == 0;
 }
 
 void DeleteStack(Stack *s) {
-    free(s->elems);
+  free(s->elems);
 }
